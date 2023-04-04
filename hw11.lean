756df6160e59860584a18cf5c9699e6f75a39987
Owills <oliver.w.marker@gmail.com>
2023-04-03 22:42:18 -0400

    Update hw11.lean

--- hw11.lean
+++ hw11.lean
@@ -7,23 +7,90 @@ require classical logic (e.g., Classical.em).
 
 -- 12 lines: does this need Classical.em?
 theorem p_iff_false: ∀ P : Prop, (P ↔ False) ↔ ¬ P :=
- by sorry
+ by intros p 
+    constructor
+    case mp => intros h
+               cases h 
+               case intro => assumption
+    case mpr => intros h
+                constructor
+                case mp => intros np 
+                           apply h 
+                           assumption
+                case mpr => intros f
+                            cases f                     
 
 -- 9 lines: does this need Classical.em?
 theorem p_and_not_p_eq_false: ∀ P : Prop, (P ∧ ¬ P) ↔ False :=
- by sorry
+ by intros p 
+    constructor
+    case mp => intros h
+               cases h
+               case intro => contradiction
+    case mpr => intros f
+                cases f              
 
 -- 21 lines: does this need Classical.em?
 theorem or_absorb_and_or: ∀ P Q : Prop, P ∨ (¬ P ∧ Q) ↔ (P ∨ Q) :=
-  by sorry
+  by intros p q 
+     constructor
+     case mp => intros h 
+                cases h 
+                case inl => constructor
+                            assumption
+                case inr h => cases h
+                              case intro => apply Or.inr
+                                            assumption             
+     case mpr => intros h 
+                 cases (Classical.em p)
+                 case inl => constructor
+                             assumption
+                 case inr => apply Or.inr
+                             constructor
+                             case h.left => assumption
+                             case h.right => cases h
+                                             contradiction
+                                             assumption               
+                                 
+                   
+                                           
 
 -- 11 lines: does this need Classical.em?
 theorem redundant_and: ∀ P Q : Prop, (P ∨ Q) ∧ (P ∨ ¬ Q) ↔ P :=
- by sorry
+ by intros p q 
+    constructor
+    case mp => intros h 
+               cases h 
+               case intro l m => cases l 
+                                 assumption
+                                 cases m
+                                 assumption
+                                 contradiction
+    case mpr => intros h 
+                constructor
+                constructor
+                assumption
+                constructor
+                assumption
+                  
 
 -- 10 lines: does this need Classical.em?
 theorem exportation: ∀ A B C : Prop, (A → B → C) ↔ (A ∧ B → C) :=
- by sorry
+ by intros a b c 
+    constructor 
+    case mp => intros h m
+               apply h 
+               case a => cases m 
+                         assumption
+               case a => cases m 
+                         assumption            
+    case mpr => intros h q p 
+                apply h 
+                constructor
+                assumption
+                assumption
+  
+
     
 -- part 0
 
@@ -61,16 +128,76 @@ def my_function : ArgType -> ArgType -> RetType
 | patterns => body
 decreasing_by sorry
 -/
+inductive Color : Type where
+  | red : Color
+  | black : Color
 
--- part 1
+
+
 inductive RedBlackTree : Type where
+ | Null (c : Color) 
+ | Node (value : Nat) (c : Color) (left : RedBlackTree) (right : RedBlackTree) 
+  
 
 def RedBlackTree.empty : RedBlackTree := by sorry
 
+theorem EmptySpec : RedBlackTree.empty = RedBlackTree.Null Color.black := by sorry
+
+
+
+
+
 def RedBlackTree.insert : RedBlackTree -> Nat -> RedBlackTree := by sorry
+
+def isBlack? : (c : Color) -> Bool
+| Color.red => false
+| Color.black => true
+
+def isRed? : (c : Color) -> Bool
+| Color.red => true
+| Color.black => false
+
+def rootblack? : (rbt : RedBlackTree) -> Bool 
+| RedBlackTree.Null c       => isBlack? c 
+| RedBlackTree.Node _ c _ _ => isBlack? c 
+
+def redProperty? : (rbt : RedBlackTree) -> Bool 
+| RedBlackTree.Null _ => true
+| RedBlackTree.Node _ c l r => if isRed? c then rootblack? l /\ rootblack? r /\ redProperty? l /\ redProperty? r 
+                                           else redProperty? l /\ redProperty? r
+
+
+def numblacknodesleft : (rbt : RedBlackTree) -> Nat
+| RedBlackTree.Null _ => 0
+| RedBlackTree.Node _ c l _ => if isBlack? c then 1 + numblacknodesleft l else 0 + numblacknodesleft l 
+
+def blackPropetyHelper? : (rbt : RedBlackTree) -> (n t : Nat) -> Bool 
+| RedBlackTree.Null _, n, t      => if n = t then true else false
+| RedBlackTree.Node _ c l r, n, t => if isBlack? c then blackPropetyHelper? l n (t+1) /\ blackPropetyHelper? r n (t+1) 
+                                                   else blackPropetyHelper? l n t /\ blackPropetyHelper? r n t 
+
+def blackProperty? : (rbt : RedBlackTree) -> Bool 
+| RedBlackTree.Null _ => true
+| RedBlackTree.Node _ _ l r => blackPropetyHelper? l 0 (numblacknodesleft l) /\ blackPropetyHelper? r 0 (numblacknodesleft l)
+
+
+theorem InsertRootProp : forall (rbt : RedBlackTree) (n : Nat), rootblack? (RedBlackTree.insert rbt n) = true := by sorry
+
+theorem InsertRedProp : forall (rbt : RedBlackTree) (n : Nat), redProperty? (RedBlackTree.insert rbt n) = true := by sorry
+
+theorem InsertBlackProp : forall (rbt : RedBlackTree) (n : Nat), blackProperty? (RedBlackTree.insert rbt n) = true := by sorry
+
+
+
+
 
 def RedBlackTree.delete : RedBlackTree -> Nat -> RedBlackTree := by sorry
 
+theorem DeleteRootProp : forall (rbt : RedBlackTree) (n : Nat), rootblack? (RedBlackTree.delete rbt n) = true := by sorry
+
+theorem DeleteRedProp : forall (rbt : RedBlackTree) (n : Nat), redProperty? (RedBlackTree.delete rbt n) = true := by sorry
+
+theorem DeleteBlackProp : forall (rbt : RedBlackTree) (n : Nat), blackProperty? (RedBlackTree.delete rbt n) = true := by sorry
 -- part 1
 
 /- PROBLEM 3: Information flow
@@ -89,8 +216,13 @@ that if users see the _output_ of it they cannot determine anything
 about the private inputs.
 -/
 
+
+
 -- part 2
 def process (private_inputs : List Nat) (public_inputs : List Nat) : Nat := by sorry
+
+theorem processProp : forall (private_inputs : List Nat) (others : List Nat) (public_inputs : List Nat), 
+                             (process others public_inputs) = (process private_inputs public_inputs) := by sorry
 
 -- part 2
 
@@ -121,5 +253,15 @@ def give_mortgage : List Nat -- income
                  -> Nat      -- age
                  -> String   -- zipcode
                  -> Bool := by sorry
+
+
+
+theorem give_mortgageProp : forall (income : List Nat) (assets : Nat)
+                                   (race : String) (otherRace : String)
+                                   (gender : String)  (otherString : String)
+                                   (age : Nat)  (otherAge : Nat)
+                                   (zip : String)  (otherZip : String),  
+                                   (give_mortgage income assets race gender age zip) = 
+                                   (give_mortgage income assets otherRace otherGender otherAge otherZip) := by sorry
 
 -- part 3
